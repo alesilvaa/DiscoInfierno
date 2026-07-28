c1fa9f1ffda6c37a4b3b06ec93353c7748fdcb89
Alejandro <alejandroacostasilvapy@gmail.com>
2026-07-28 18:10:32 -0300

    Nueva scene 2D, ui agregados y logica general

--- Assets/DiscoInfierno/Scripts/GameController.cs
+++ Assets/DiscoInfierno/Scripts/GameController.cs
@@ -1,16 +1,61 @@
+using System;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class GameController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public static GameController Instance { get; private set; }
+
+    [Header("Progreso del nivel")]
+    [SerializeField, Min(1)] int cubesRequiredForExit = 5;
+    [SerializeField] PrefabGrid2D grid;
+    [SerializeField] UnityEvent onExitUnlocked;
+
+    [SerializeField] int destroyedCubes;
+    [SerializeField] bool exitUnlocked;
+
+    public int DestroyedCubes => destroyedCubes;
+    public int CubesRequiredForExit => cubesRequiredForExit;
+    public bool ExitUnlocked => exitUnlocked;
+    public event Action<int, int> KillCountChanged;
+
+    void Awake()
     {
-        
+        if (Instance != null && Instance != this)
+        {
+            Debug.LogWarning("Hay más de un GameController activo en la escena.", this);
+            return;
+        }
+
+        Instance = this;
+        if (grid == null)
+            grid = FindFirstObjectByType<PrefabGrid2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
-        
+        destroyedCubes = 0;
+        exitUnlocked = false;
+        grid?.LockExit();
+        KillCountChanged?.Invoke(destroyedCubes, cubesRequiredForExit);
+    }
+
+    void OnDestroy()
+    {
+        if (Instance == this)
+            Instance = null;
+    }
+
+    public void RegisterDestroyedCube()
+    {
+        destroyedCubes++;
+        KillCountChanged?.Invoke(destroyedCubes, cubesRequiredForExit);
+
+        if (exitUnlocked || destroyedCubes < cubesRequiredForExit)
+            return;
+
+        exitUnlocked = true;
+        grid?.RevealExit();
+        onExitUnlocked?.Invoke();
     }
 }
